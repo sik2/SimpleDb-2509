ce1c3a7932851eea044f5e1eb9f2a9a83617327c
yunabin <yhb1020525@gmail.com>
2026-05-19 17:08:02 +0900

    feat: Article 클래스 및 SimpleDb 클래스 도입

---
+++ src/main/java/com/back/simpleDb/SimpleDb.java
@@ -0,0 +1,92 @@
+package com.back.simpleDb;
+
+import java.sql.*;
+
+public class SimpleDb {
+    private final String host;
+    private final String user;
+    private final String password;
+    private final String dbName;
+    private boolean devMode;
+
+    private final ThreadLocal<Connection> connectionHolder = new ThreadLocal<>();
+
+    public SimpleDb(String host, String user, String password, String dbName) {
+        this.host = host;
+        this.user = user;
+        this.password = password;
+        this.dbName = dbName;
+    }
+
+    public void setDevMode(boolean devMode) {
+        this.devMode = devMode;
+    }
+
+    public Connection getConnection() {
+        try {
+            Connection conn = connectionHolder.get();
+            if (conn == null || conn.isClosed()) {
+                conn = DriverManager.getConnection(
+                        "jdbc:mysql://" + host + "/" + dbName +
+                                "?serverTimezone=Asia/Seoul",
+                        user, password
+                );
+                connectionHolder.set(conn);
+            }
+            return conn;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Sql genSql() {
+        return new Sql(getConnection(), devMode);
+    }
+
+    public void run(String sql, Object... params) {
+        Sql s = new Sql(getConnection(), devMode);
+        s.append(sql, params);
+        s.run();
+    }
+
+    public void close() {
+        Connection conn = connectionHolder.get();
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            } finally {
+                connectionHolder.remove();
+            }
+        }
+    }
+
+    public void startTransaction() {
+        try {
+            getConnection().setAutoCommit(false);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void commit() {
+        try {
+            Connection conn = getConnection();
+            conn.commit();
+            conn.setAutoCommit(true);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void rollback() {
+        try {
+            Connection conn = getConnection();
+            conn.rollback();
+            conn.setAutoCommit(true);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

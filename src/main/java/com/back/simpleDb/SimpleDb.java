25539f4ef0f9fe25b2542139a485c210bfd10fc0
predevho <dlagusgh7894@gmail.com>
2026-05-26 16:27:07 +0900

    feat: 기본 생성자 구현

--- src/main/java/com/back/simpleDb/SimpleDb.java
+++ src/main/java/com/back/simpleDb/SimpleDb.java
@@ -1,8 +1,17 @@
 package com.back.simpleDb;
 
+
 public class SimpleDb {
+    private final String host;
+    private final String user;
+    private final String password;
+    private final String database;
 
     public SimpleDb(String host, String username, String password, String database) {
+        this.host = host;
+        this.user = username;
+        this.password = password;
+        this.database = database;
     }
 
     public void setDevMode(boolean devMode) {

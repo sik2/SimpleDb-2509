68f8d35ce8f33dd91d4b163fd9a77b35815b36b2
Rakhyunn <krh0323@pukyong.ac.kr>
2026-05-27 20:22:22 +0900

    [feat] 테스트 세팅 구현

---
+++ src/main/java/com/back/Article.java
@@ -0,0 +1,17 @@
+package com.back;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class Article {
+    private Long id;
+    private String title;
+    private String body;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+    private boolean isBlind;
+}
---
+++ src/main/java/com/back/simpleDb/SimpleDb.java
@@ -0,0 +1,42 @@
+package com.back.simpleDb;
+
+import lombok.Setter;
+
+import java.sql.*;
+
+public class SimpleDb {
+    private final String host;
+    private final String user;
+    private final String password;
+    private final String dbName;
+    @Setter
+    private boolean devMode = false;
+
+    public SimpleDb(String host, String user, String password, String dbName) {
+        this.host = host;
+        this.user = user;
+        this.password = password;
+        this.dbName = dbName;
+    }
+
+    private Connection getConnection() throws SQLException {
+        String url = "jdbc:mysql://" + host + "/" + dbName + "?useSSL=false&allowPublicKeyRetrieval=true";
+        return DriverManager.getConnection(url, user, password);
+    }
+
+    public void run(String query, Object... params) {
+        if (devMode) System.out.println("[SQL] " + query);
+
+        try (Connection conn = getConnection();
+             PreparedStatement ps = conn.prepareStatement(query)) {
+
+            for (int i = 0; i < params.length; i++) {
+                ps.setObject(i + 1, params[i]);
+            }
+            ps.execute();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
